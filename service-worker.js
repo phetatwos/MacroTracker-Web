531bf77020ec993d284e1d1b5955fc9f04542580
fefe <phetatwo@gmail.com>
2026-04-22 17:27:35 -0400

    Deploy update: Wed Apr 22 17:27:35 EDT 2026

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'macro-tracker-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './composeApp.js',
+  './composeApp.wasm',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
